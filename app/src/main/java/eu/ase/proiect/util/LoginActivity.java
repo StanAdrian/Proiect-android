55adc429d3f454133d954910829d3aba7a77f0c2
Stan Sebastian <sebyiky@gmail.com>
2020-12-26 20:23:37 +0200

    Am facut sistem de register si log in

---
+++ app/src/main/java/eu/ase/proiect/util/LoginActivity.java
@@ -0,0 +1,120 @@
+package eu.ase.proiect.util;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import eu.ase.proiect.MainActivity;
+import eu.ase.proiect.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText nume_utilizator,email_utilizator,parola_utilizator;
+    private Button btn_log_in,btn_sign_up;
+    private ProgressBar baraProgresLogIN;
+    private ImageView imagine_utilizator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        nume_utilizator=findViewById(R.id.log_username);
+        email_utilizator=findViewById(R.id.log_username);
+        parola_utilizator=findViewById(R.id.log_password);
+        btn_log_in=findViewById(R.id.log_in_btn);
+        btn_sign_up=findViewById(R.id.log_signup_btn);
+        baraProgresLogIN=findViewById(R.id.log_progressBar);
+        imagine_utilizator=findViewById(R.id.log_ImageView);
+        mAuth=FirebaseAuth.getInstance();
+
+        imagine_utilizator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //cand da clic pe imagine sa isi faca cont, o sa vad cum fac ca atunci cand da pe imagine sa aleaga din mai multe conturi si sa le apara imaginea
+                Intent regActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(regActivity);
+                finish();
+            }
+        });
+        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //aici se face mutarea pe pagina de inregistrare
+                Intent regActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(regActivity);
+                finish();
+            }
+        });
+        btn_log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                baraProgresLogIN.setVisibility(View.VISIBLE);
+                btn_log_in.setVisibility(View.INVISIBLE);
+                final String mail=email_utilizator.getText().toString();
+                final String parola=parola_utilizator.getText().toString();
+                if (mail.isEmpty() || parola.isEmpty()){
+                    arataMesaj("Verificati campurile!");
+                }
+                else {
+                    signIN(mail,parola);
+                }
+            }
+        });
+    }
+
+    private void signIN(String mail, String parola) {
+        //folosim functiile din firebase pt login
+    mAuth.signInWithEmailAndPassword(mail,parola).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful())
+            {
+                baraProgresLogIN.setVisibility(View.INVISIBLE);
+                btn_log_in.setVisibility(View.VISIBLE);
+                //functie de trimitere in activitatea principala
+                updateUI();
+            }
+            else {
+                arataMesaj(task.getException().getMessage());
+                baraProgresLogIN.setVisibility(View.INVISIBLE);
+                btn_log_in.setVisibility(View.VISIBLE);
+            }
+        }
+    });
+    }
+
+    private void updateUI() {
+        Intent homeActivity = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private void arataMesaj(String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user=mAuth.getCurrentUser();
+        if (user != null){
+            //userul este deja conectat deci il trimitem in ecranul principal
+            updateUI();
+        }
+    }
+}
---
+++ app/src/main/java/eu/ase/proiect/util/RegisterActivity.java
@@ -0,0 +1,193 @@
+package eu.ase.proiect.util;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import eu.ase.proiect.MainActivity;
+import eu.ase.proiect.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    static int PCodeRequest=1;
+    static int CodeRequest=1;
+    ImageView pozaUtilizator;
+    Uri pickedImgUri;
+    private EditText nume_utilizator,email_utilizator,parola_utilizator,parola2_utilizator;
+    private ProgressBar loadingProgress;
+
+    private FirebaseAuth autentificare_firebase;
+    private Button button_register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //inu views
+        nume_utilizator=findViewById(R.id.reg_Username);
+        email_utilizator=findViewById(R.id.reg_Email);
+        parola_utilizator=findViewById(R.id.reg_Password);
+        parola2_utilizator=findViewById(R.id.reg_Password2);
+        loadingProgress=findViewById(R.id.reg_progressBar);
+        loadingProgress.setVisibility(View.INVISIBLE);
+
+        autentificare_firebase = FirebaseAuth.getInstance();
+        button_register=findViewById(R.id.reg_button);
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button_register.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String nume=nume_utilizator.getText().toString();
+                final String email=email_utilizator.getText().toString();
+                final String parola=parola_utilizator.getText().toString();
+                final String parola2=parola2_utilizator.getText().toString();
+
+                if (nume.isEmpty() || email.isEmpty() || parola.isEmpty() || !parola.equals(parola2)){
+                    //punem mesaj de eroare pt ca credentialele de inregistrare sunt prost completate
+                    arataMesaj("Verificati si completati toate campurile!");
+                    button_register.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    //creem metoda de creeare cont
+                    CreeazaCont(nume,email,parola);
+                }
+            }
+        });
+
+        pozaUtilizator = findViewById(R.id.reg_ImageView);
+        pozaUtilizator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >=22){
+                    verificaSiCerePermisiune();
+                }
+                else {
+                    deschideGaleria();
+                }
+            }
+        });
+    }
+
+    private void CreeazaCont(final String nume, String email, String parola) {
+        autentificare_firebase.createUserWithEmailAndPassword(email,parola)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //utilizatorul a fost creat cu succes
+                            arataMesaj("Cont Creeat!");
+                            //acum updatam contul utilizatorului cu numele si poza
+                            UpdateUserInfo( nume ,pickedImgUri,autentificare_firebase.getCurrentUser());
+
+                        }
+                        else {
+                            arataMesaj("creearea a dat gres!");
+                            button_register.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+    }
+
+    private void UpdateUserInfo(final String name, Uri pickedImgUri, final FirebaseUser currentUser){
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("poze_utilizatori");
+        final StorageReference imageFIlePath=mStorage.child(pickedImgUri.getLastPathSegment());
+        imageFIlePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //imaginea a fost uploadata cu succes
+                //acum obtinem url-ul imaginiii
+                imageFIlePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        //uri contine url-ul imaginii
+                        UserProfileChangeRequest profileUpadte = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+                        currentUser.updateProfile(profileUpadte).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    //s-a updatata informatia utilizatorului cu succes
+                                    arataMesaj("Inregsitrare Completa");
+                                    updateUI();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private void updateUI() {
+        Intent homeActivity = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private void arataMesaj(String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    private void deschideGaleria() {
+        Intent galerieIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galerieIntent.setType("image/*");
+        startActivityForResult(galerieIntent,CodeRequest);
+    }
+
+    private void verificaSiCerePermisiune() {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(RegisterActivity.this,"Acceptati permisiunile necesare",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                ActivityCompat.requestPermissions(RegisterActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PCodeRequest);
+            }
+        }
+        else {
+            deschideGaleria();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode==CodeRequest && data != null ){
+            // utilizatorul a ales cu succes o imagine
+            // avem nevoie de referinta variabilei Uri a imaginii
+            pickedImgUri = data.getData();
+            pozaUtilizator.setImageURI(pickedImgUri);
+        }
+    }
+}
